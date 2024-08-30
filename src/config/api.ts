f5a3399e5001841450f85c2481a2f35fd462ed35
nhoclahola <nhoclahola@gmail.com>
2024-08-30 23:12:22 +0700

    Update API URL using environment variables
    API BASE URL in api.ts now uses REACT_APP_API_BASE_URL from the environment  variables to be able to change flexibly when deploying.

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8080";
+console.log(process.env.REACT_APP_API_BASE_URL);
+
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 
 const jwt = localStorage.getItem("jwt");
 
