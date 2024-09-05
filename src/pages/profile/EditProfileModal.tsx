ba84107f9fc16888a1340b7727c6ee835663599f
nhoclahola <nhoclahola@gmail.com>
2024-09-05 22:24:46 +0700

    Fix Update Profile function

--- src/pages/profile/EditProfileModal.tsx
+++ src/pages/profile/EditProfileModal.tsx
@@ -46,86 +46,6 @@ interface FormValues {
 
 const EditProfileModal = ({ open, handleClose, setUser }: EditProfileModalProps) => {
   const auth = useAppSelector((store) => store.auth);
-  const [avatar, setAvatar] = React.useState<File | null>(null);
-  const [cover, setCover] = React.useState<File | null>(null);
-  const [avatarObjectUrl, setAvatarObjectUrl] = React.useState<string | undefined>(undefined);
-  const [coverObjectUrl, setCoverObjectUrl] = React.useState<string | undefined>(undefined);
-
-
-  const handleAvatarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files[0]) {
-      setAvatar(event.target.files[0]);
-    }
-  };
-
-  const handleCoverChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files[0]) {
-      setCover(event.target.files[0]);
-    }
-  };
-
-  // Clean up if image change
-  React.useEffect(() => {
-    let objectUrl: string | null = null;
-    if (avatar) {
-      objectUrl = URL.createObjectURL(avatar);
-      setAvatarObjectUrl(objectUrl);
-    }
-    return () => {
-      if (objectUrl) {
-        URL.revokeObjectURL(objectUrl);
-        console.log("revoked avatar url", objectUrl);
-      }
-    };
-  }, [avatar]);
-
-  React.useEffect(() => {
-    let objectUrl: string | null = null;
-    if (cover) {
-      objectUrl = URL.createObjectURL(cover);
-      setCoverObjectUrl(objectUrl);
-    }
-    return () => {
-      if (objectUrl) {
-        URL.revokeObjectURL(objectUrl);
-        console.log("revoked cover url", objectUrl);
-      }
-    };
-  }, [cover]);
-
-  const uploadAvatar = async (image: File | null) => {
-    const formData = new FormData();
-    // If avatar is null, append an empty file to prevent error
-    const emptyFile = new File([], '');
-    formData.append('image', image || emptyFile);
-    axios.post(`${API_BASE_URL}/api/users/avatar`, formData, {
-      headers: {
-        "Authorization": `Bearer ${localStorage.getItem("jwt")}`,
-        "Content-Type": "multipart/form-data"
-      }
-    }).then((response) => {
-      console.log(response);
-    }).catch((error) => {
-      console.error("error", error);
-    });
-  };
-
-  const uploadCover = async (image: File | null) => {
-    const formData = new FormData();
-    // If avatar is null, append an empty file to prevent error
-    const emptyFile = new File([], '');
-    formData.append('image', image || emptyFile);
-    axios.post(`${API_BASE_URL}/api/users/cover`, formData, {
-      headers: {
-        "Authorization": `Bearer ${localStorage.getItem("jwt")}`,
-        "Content-Type": "multipart/form-data"
-      }
-    }).then((response) => {
-      console.log(response)
-    }).catch((error) => {
-      console.error("error", error);
-    });
-  };
 
   const errorFromStore = useSelector((state: RootState) => state.auth.error);
   const [error, setError] = React.useState<string | null>(null);
@@ -145,15 +65,6 @@ const EditProfileModal = ({ open, handleClose, setUser }: EditProfileModalProps)
     },
     onSubmit: async (values) => {
       await dispatch(updateProfileAction(values));
-
-      const uploadPromises = [];
-      if (avatar) {
-        uploadPromises.push(uploadAvatar(avatar));
-      }
-      if (cover) {
-        uploadPromises.push(uploadCover(cover));
-      }
-      await Promise.all(uploadPromises);
       window.location.reload();
     }
   })
@@ -162,16 +73,6 @@ const EditProfileModal = ({ open, handleClose, setUser }: EditProfileModalProps)
     handleClose();
     setError(null); // Clear error state
   };
-
-  React.useEffect(() => {
-    if (avatarObjectUrl)
-      console.log(avatarObjectUrl)
-  }, [avatarObjectUrl]);
-
-  React.useEffect(() => {
-    if (coverObjectUrl)
-      console.log(coverObjectUrl)
-  }, [coverObjectUrl]);
 
   return (
     <div>
@@ -198,33 +99,6 @@ const EditProfileModal = ({ open, handleClose, setUser }: EditProfileModalProps)
               </div>
               <h1 className="text-center font-bold text-xl mb-5">Edit Profile</h1>
               {error && <div className="text-red-500 text-center">{error}</div>}
-              <div className="relative">
-                <div className="relative">
-                  <input className="hidden" placeholder="image" id="cover-input" type="file" accept="image/*" onChange={handleCoverChange} ></input>
-                  <div className="h-[10rem] bg-slate-400">
-                    {coverObjectUrl ? <img src={coverObjectUrl} alt="cover" className="w-full h-full" /> : auth?.user?.coverPhotoUrl ? <img src={auth?.user.coverPhotoUrl} alt="cover" className="w-full h-full" /> : null}
-                  </div>
-                  <label className="cursor-pointer" htmlFor="cover-input">
-                    <div className="absolute right-2 bottom-4 bg-slate-50 px-2 py-1 rounded-lg flex items-center hover:bg-transparent hover:text-orange-600">
-                      <ImageIcon />
-                      <span>Change cover</span>
-                    </div>
-                  </label>
-                </div>
-                <div className="pl-5 h-[3rem]">
-                  <input className="hidden" placeholder="image" id="avatar-input" type="file" accept="image/*" onChange={handleAvatarChange} ></input>
-                  <div className="absolute bottom-0.5">
-                    <Avatar className="outline outline-3 outline-slate-300" sx={{ width: "8rem", height: "8rem" }}>
-                      {avatarObjectUrl ? <img src={avatarObjectUrl} alt="avatar" className="h-full w-auto object-cover" /> : auth?.user?.avatarUrl ? <img src={auth?.user.avatarUrl} alt="avatar" className="h-full w-auto object-cover" /> : null}
-                    </Avatar>
-                    <label htmlFor="avatar-input">
-                      <IconButton sx={{ bgcolor: "#beccc2" }} className="hover:text-orange-600 absolute left-24 bottom-10" component="span" >
-                        <CameraAltIcon></CameraAltIcon>
-                      </IconButton>
-                    </label>
-                  </div>
-                </div>
-              </div>
               <div className="space-y-3">
                 <TextField fullWidth id="firstName" name="firstName" label="First Name" value={formik.values.firstName} onChange={formik.handleChange} />
                 <TextField fullWidth id="lastName" name="lastName" label="Last Name" value={formik.values.lastName} onChange={formik.handleChange} />
--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Button, Card, Tab, Tabs, useTheme } from "@mui/material";
+import { Avatar, Box, Button, Card, CircularProgress, IconButton, Tab, Tabs, useTheme } from "@mui/material";
 import React from 'react'
 import { Link, Navigate, Outlet, Route, Routes, useLocation, useNavigate, useParams } from "react-router-dom";
 import ProfilePosts from "./ProfilePosts";
@@ -12,6 +12,8 @@ import axios from "axios";
 import { API_BASE_URL } from "../../config/api";
 import Loading from "./Loading";
 import UserWithData from "../../utils/UserWithData";
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import ImageIcon from '@mui/icons-material/Image';
 
 const tabs = [
   { value: "posts", name: "Posts" },
@@ -52,8 +54,8 @@ const Profile = () => {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-	const [isFollow, setIsFollow] = React.useState<boolean>(false);
-	const [followerCount, setFollowerCount] = React.useState<number>(0);
+  const [isFollow, setIsFollow] = React.useState<boolean>(false);
+  const [followerCount, setFollowerCount] = React.useState<number>(0);
 
   // Get user information
   React.useEffect(() => {
@@ -67,7 +69,7 @@ const Profile = () => {
       setUser(response.data.result);
       console.log(user);
       setIsFollow(response.data.result.follow);
-			setFollowerCount(response.data.result.followers);
+      setFollowerCount(response.data.result.followers);
       setLoading(false);
     }).catch(error => {
       console.error(error);
@@ -75,64 +77,253 @@ const Profile = () => {
     })
   }, [userId])
 
+  //                            Change avatar, cover photo function
+  const [avatar, setAvatar] = React.useState<File | null>(null);
+  const [loadingAvatar, setLoadingAvatar] = React.useState(false);
+  const [cover, setCover] = React.useState<File | null>(null);
+  const [loadingCover, setLoadingCover] = React.useState(false);
+  const [avatarObjectUrl, setAvatarObjectUrl] = React.useState<string | undefined>(undefined);
+  const [coverObjectUrl, setCoverObjectUrl] = React.useState<string | undefined>(undefined);
+
+  const handleAvatarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setAvatar(event.target.files[0]);
+    }
+  };
+
+  const handleCoverChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setCover(event.target.files[0]);
+    }
+  };
+
+  // Clean up if image change
+  React.useEffect(() => {
+    let objectUrl: string | null = null;
+    if (avatar) {
+      objectUrl = URL.createObjectURL(avatar);
+      setAvatarObjectUrl(objectUrl);
+    }
+    return () => {
+      if (objectUrl) {
+        URL.revokeObjectURL(objectUrl);
+        console.log("revoked avatar url", objectUrl);
+      }
+    };
+  }, [avatar]);
+
+  React.useEffect(() => {
+    let objectUrl: string | null = null;
+    if (cover) {
+      objectUrl = URL.createObjectURL(cover);
+      setCoverObjectUrl(objectUrl);
+    }
+    return () => {
+      if (objectUrl) {
+        URL.revokeObjectURL(objectUrl);
+        console.log("revoked cover url", objectUrl);
+      }
+    };
+  }, [cover]);
+
+  React.useEffect(() => {
+    setAvatar(null);
+    setCover(null);
+    setAvatarObjectUrl(undefined);
+    setCoverObjectUrl(undefined);
+  }, [user]);
+
+  const uploadAvatar = async (image: File | null) => {
+    setLoadingAvatar(true);
+    const formData = new FormData();
+    // If avatar is null, append an empty file to prevent error
+    const emptyFile = new File([], '');
+    formData.append('image', image || emptyFile);
+    axios.post(`${API_BASE_URL}/api/users/avatar`, formData, {
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem("jwt")}`,
+        "Content-Type": "multipart/form-data"
+      }
+    }).then((response) => {
+      window.location.reload();
+    }).catch((error) => {
+      setLoadingAvatar(false);
+      setAvatar(null);
+      console.error("error", error);
+    });
+  };
+
+  const uploadCover = async (image: File | null) => {
+    setLoadingCover(true);
+    const formData = new FormData();
+    // If cover is null, append an empty file to prevent error
+    const emptyFile = new File([], '');
+    formData.append('image', image || emptyFile);
+    axios.post(`${API_BASE_URL}/api/users/cover`, formData, {
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem("jwt")}`,
+        "Content-Type": "multipart/form-data"
+      }
+    }).then((response) => {
+      window.location.reload();
+    }).catch((error) => {
+      setLoadingCover(false);
+      setCover(null);
+      console.error("error", error);
+    });
+  };
+
   if (loading)
     return (
       <Loading />
     )
-	
-	const followUser = () => {
-		axios.post(`/api/users/follow/${userId}`, {}, {
-			baseURL: API_BASE_URL,
-			headers: {
-				"Authorization": `Bearer ${localStorage.getItem("jwt")}`,
-			}
-		}).then(response => {
-			if (response.data.result === "followed") {
-				setIsFollow(true);
-				setFollowerCount((prev) => prev + 1);
-			}
-			else if (response.data.result === "unfollowed") {
-				setIsFollow(false);
-				setFollowerCount((prev) => prev - 1);
-			}
-		}).catch((error) => {
-			console.error("error", error);
-		})
-	}
+
+  const followUser = () => {
+    axios.post(`/api/users/follow/${userId}`, {}, {
+      baseURL: API_BASE_URL,
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem("jwt")}`,
+      }
+    }).then(response => {
+      if (response.data.result === "followed") {
+        setIsFollow(true);
+        setFollowerCount((prev) => prev + 1);
+      }
+      else if (response.data.result === "unfollowed") {
+        setIsFollow(false);
+        setFollowerCount((prev) => prev - 1);
+      }
+    }).catch((error) => {
+      console.error("error", error);
+    })
+  }
+
+  const cancelSaveAvatar = () => {
+    setAvatar(null);
+  }
+
+  const cancelSaveCover = () => {
+    setCover(null);
+  }
 
   return (
-    <div className="pb-10 w-full px-5">
-    <div style={{backgroundColor: theme.palette.background.paper}}>
-      <div onDragStart={stopDragging}  className="h-[15rem] bg-slate-400 cursor-pointer">
-        {user?.coverPhotoUrl && <img alt="cover" className="w-full h-full rounded-md" src={user?.coverPhotoUrl}></img>}
-      </div>
-      <div className="px-5 flex justify-between items-start mt-5 h-[4rem]">
-        <Avatar onDragStart={stopDragging} className="transform -translate-y-24 outline outline-4 outline-slate-300 cursor-pointer" sx={{ width: "10rem", height: "10rem" }}>
-          {user?.avatarUrl && <img src={user.avatarUrl} alt="avatar" className="h-full w-auto object-cover" />}
-        </Avatar>
-        {auth?.user?.userId === userId ? <Button onClick={handleOpen} sx={{ borderRadius: "40px" }} variant="outlined">Edit Profile</Button> : <Button onClick={followUser} sx={{ borderRadius: "40px" }} variant="outlined">{isFollow ? "Unfollow" : "Follow"}</Button>}
-      </div>
+    <div className="relative pb-10 w-full px-5">
       {
-      user &&
-        <section className="p-5">
-          <div>
-            <h1 className="py-1 font-bold text-xl">{user?.firstName + " " + user?.lastName}</h1>
-            <p>@{user?.username}</p>
+        cover &&
+        <div className="flex justify-end">
+          <div className="absolute top-2 z-10 space-x-4">
+            <button onClick={cancelSaveCover} style={{ backgroundColor: theme.palette.background.paper }} className="hover:text-red-500 rounded-md w-20">
+              <span className="p-2">Cancel</span>
+            </button>
+            <button onClick={() => uploadCover(cover)} style={{ backgroundColor: theme.palette.background.paper }} className="hover:text-cyan-500 rounded-md w-20">
+              <span className="p-2">Save</span>
+            </button>
           </div>
-          <div className="flex gap-2 items-center py-3">
-            <span>{user?.posts} Posts</span>
-            <Link to={`/profile/${user.userId}/followers`}>
-              <span className="font-bold hover:underline">{followerCount} Followers</span>
-            </Link>
-            <Link to={`/profile/${user.userId}/following`}>
-              <span className="font-bold hover:underline">{user?.followings} Followings</span>
-            </Link>
-          </div>
-          <div>
-            <p>{user?.description}</p>
-          </div>
-        </section>
+        </div>
       }
+      <div style={{ backgroundColor: theme.palette.background.paper }}>
+        <div className="relative">
+          <div className="relative">
+            <input className="hidden" placeholder="image" id="cover-input" type="file" accept="image/*" onChange={handleCoverChange} ></input>
+            <div onDragStart={stopDragging} className="h-[15rem] bg-slate-400 cursor-pointer">
+              {cover ? <img src={coverObjectUrl} alt="cover" className="w-full h-full" /> : user?.coverPhotoUrl && <img alt="cover" className="w-full h-full rounded-md" src={user?.coverPhotoUrl}></img>}
+            </div>
+            {
+              auth?.user?.userId === userId &&
+              <label className="cursor-pointer" htmlFor="cover-input">
+                <div style={{ backgroundColor: theme.palette.background.paper }} className="absolute right-2 bottom-4 px-2 py-1 rounded-lg flex items-center hover:text-cyan-600">
+                  <ImageIcon />
+                  <span>Change cover</span>
+                </div>
+              </label>
+            }
+            {
+              loadingCover &&
+              <div className="absolute top-1/2 left-1/2">
+                <CircularProgress />
+              </div>
+            }
+          </div>
+          <div className="px-5 flex justify-between items-start mt-5 h-[4rem]">
+            <input className="hidden" placeholder="image" id="avatar-input" type="file" accept="image/*" onChange={handleAvatarChange} ></input>
+            <div className="absolute bottom-[-0.5rem]">
+              <Avatar onDragStart={stopDragging} className="outline outline-4 outline-slate-300 cursor-pointer" sx={{ width: "10rem", height: "10rem" }}>
+                {avatar ? <img src={avatarObjectUrl} alt="avatar" className="h-full w-auto object-cover" /> : user?.avatarUrl && <img src={user.avatarUrl} alt="avatar" className="h-full w-auto object-cover" />}
+              </Avatar>
+              {
+                loadingAvatar &&
+                <div className="absolute top-1/3 left-[40%]">
+                  <CircularProgress />
+                </div>
+              }
+              {
+                auth?.user?.userId === userId &&
+                <label className="absolute left-28 bottom-0" htmlFor="avatar-input">
+                  <IconButton sx={{ bgcolor: "#beccc2" }} className="hover:text-cyan-500" component="span" >
+                    <CameraAltIcon></CameraAltIcon>
+                  </IconButton>
+                </label>
+              }
+            </div>
+            <div className="flex w-full justify-end gap-x-10">
+              {
+                avatar &&
+                <div className="space-x-4">
+                  <Button onClick={cancelSaveAvatar}
+                    sx={{
+                      borderRadius: "40px",
+                      borderColor: "red",
+                      color: "red",
+                      '&:hover': {
+                        borderColor: "firebrick ",
+                        color: "firebrick ",
+                      },
+                    }}
+                    variant="outlined">
+                    Cancel
+                  </Button>
+                  <Button
+                    onClick={() => uploadAvatar(avatar)}
+                    variant="outlined"
+                    sx={{
+                      borderRadius: "40px",
+                      borderColor: "limegreen", 
+                      color: "limegreen",  
+                      '&:hover': {
+                        borderColor: "mediumseagreen", 
+                        color: "mediumseagreen", 
+                      },
+                    }}
+                  >
+                    Save Avatar
+                  </Button>
+                </div>
+              }
+              {auth?.user?.userId === userId ? <Button onClick={handleOpen} sx={{ borderRadius: "40px" }} variant="outlined">Edit Profile</Button> : <Button onClick={followUser} sx={{ borderRadius: "40px" }} variant="outlined">{isFollow ? "Unfollow" : "Follow"}</Button>}
+            </div>
+          </div>
+        </div>
+        {
+          user &&
+          <section className="p-5">
+            <div>
+              <h1 className="py-1 font-bold text-xl">{user?.firstName + " " + user?.lastName}</h1>
+              <p>@{user?.username}</p>
+            </div>
+            <div className="flex gap-2 items-center py-3">
+              <span>{user?.posts} Posts</span>
+              <Link to={`/profile/${user.userId}/followers`}>
+                <span className="font-bold hover:underline">{followerCount} Followers</span>
+              </Link>
+              <Link to={`/profile/${user.userId}/following`}>
+                <span className="font-bold hover:underline">{user?.followings} Followings</span>
+              </Link>
+            </div>
+            <div>
+              <p>{user?.description}</p>
+            </div>
+          </section>
+        }
       </div>
       {user ?
         <section>
