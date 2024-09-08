99e840454b7d87cb5ebf833e7e40bff466ce518d
nhoclahola <nhoclahola@gmail.com>
2024-09-08 21:51:44 +0700

    Update Convert Day, now it will use UTC0 and should be convert to your time zone

--- src/utils/ConvertDate.ts
+++ src/utils/ConvertDate.ts
@@ -1,19 +1,21 @@
 export default function formatDateFromString(dateString: string): string {
-	const date = new Date(dateString); // Chuyển đổi chuỗi ngày tháng thành đối tượng Date
+	// Add 'Z' at the end of the string to indicate that dateString is in UTC
+	const utcDateString = `${dateString}Z`;
+	const date = new Date(utcDateString); // Convert the date string to a Date object
 	const now = new Date();
-	const diffMs = now.getTime() - date.getTime(); // Sự chênh lệch tính bằng milliseconds
-	const diffSec = Math.floor(diffMs / 1000); // Chênh lệch tính bằng seconds
-	const diffMin = Math.floor(diffSec / 60); // Chênh lệch tính bằng minutes
-	const diffHour = Math.floor(diffMin / 60); // Chênh lệch tính bằng hours
-	const diffDay = Math.floor(diffHour / 24); // Chênh lệch tính bằng days
+	const diffMs = now.getTime() - date.getTime(); // Difference in milliseconds
+	const diffSec = Math.floor(diffMs / 1000); // Difference in seconds
+	const diffMin = Math.max(0, Math.floor(diffSec / 60)); // Difference in minutes, ensuring it's not negative
+	const diffHour = Math.floor(diffMin / 60); // Difference in hours
+	const diffDay = Math.floor(diffHour / 24); // Difference in days
 
 	if (diffMin < 60) {
-		return `${diffMin} minutes ago`;
+			return `${diffMin} minutes ago`;
 	} else if (diffHour < 24) {
-		return `${diffHour} hours ago`;
+			return `${diffHour} hours ago`;
 	} else if (diffDay < 30) {
-		return `${diffDay} days ago`;
+			return `${diffDay} days ago`;
 	} else {
-		return date.toISOString();
+			return date.toISOString();
 	}
 }
